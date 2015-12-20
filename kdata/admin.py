fb5a2f83349cd8575eacb28a4be68ade2f619b5a
Richard Darst <rkd@zgib.net>
2015-12-20 12:26:02 +0200

    Add basic admin site models

--- kdata/admin.py
+++ kdata/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_id', 'user', 'name', 'type', 'active')
+admin.site.register(models.Device, DeviceAdmin)
+class DataAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Data, DataAdmin)
