a0af939c9744b13d18c3fe8217bd1381a80e959d
Richard Darst <rkd@zgib.net>
2016-01-11 17:40:14 +0200

    Add kdata middleware module

---
+++ kdata/middleware.py
@@ -0,0 +1,20 @@
+from django.core.urlresolvers import reverse
+from . import views as kviews
+from . import models
+
+
+class KdataMiddleware(object):
+    def process_view(self, request, view_func, args, kwargs):
+
+        if 'device_id' in kwargs:
+            request.breadcrumbs = breadcrumbs = [ ]
+
+            device_id = kwargs['device_id']
+            breadcrumbs.append(('All Devices', reverse('device-list')))
+            device = models.Device.objects.get(device_id=device_id)
+            breadcrumbs.append((device.name, reverse('device', kwargs={'device_id':device_id})))
+            #import IPython ; IPython.embed()
+            if 'converter' in kwargs:
+                breadcrumbs.append((kwargs['converter'], reverse('device-data',
+                                                                 kwargs=dict(device_id=device_id,
+                                                                             converter=kwargs['converter']))))
