24fabaa802628c1fd627171ee1e43267f0ec8f0b
Richard Darst <rkd@zgib.net>
2016-03-24 16:07:42 +0200

    kdata/devices.py: Add TestSurvey1 as default device
    - May as well let people experience it.

--- kdata/devices.py
+++ kdata/devices.py
@@ -24,6 +24,7 @@ standard_device_choices = [
     ('PurpleRobot', 'Purple Robot (Android)'),
     ('Ios', 'IOS'),
     ('MurataBSN', 'Murata Bed Sensor'),
+    ('kdata.survey.TestSurvey1', 'Test Survey #1'),
     #('', ''),
     #('', ''),
     ]
