ae73c557a4790d340734624eee6bfd52dbc4d3ca
Richard Darst <rkd@zgib.net>
2018-02-06 02:16:21 +0200

    kdata/logs: Update log format, add "?" before query string

--- kdata/logs.py
+++ kdata/logs.py
@@ -16,7 +16,7 @@ def log(request, message, user=None,
     username = user.username
     ip = request.META.get('REMOTE_ADDR', None)
 
-    logger.info('%s %s %s%s o=%s op=%s u=%s data_of=%s "%s"'%(
+    logger.info('%s %s %s?%s o=%s op=%s u=%s data_of=%s "%s"'%(
         request.get_host(),
         request.method, request.path, request.META['QUERY_STRING'],
         obj, op,username,
